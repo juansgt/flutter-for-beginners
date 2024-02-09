65c60e55ce55580deb4ab5b179ecf64fc1d3a8c4
Juan Santos <juan.santos@texascontrols.com.com>
2024-02-09 13:52:04 +0100

    Añadidas imagenes desde carpetas locales en lugar de online, modificando el fichero pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -13,10 +13,8 @@ class  Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: const Center(
-        child: Image(
-          image: NetworkImage('https://images.unsplash.com/photo-1516339901601-2e1b62dc0c45?q=80&w=1942&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
-        )
+      body: Center(
+        child: Image.asset('assets/space-1.jpg'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: foo, 
